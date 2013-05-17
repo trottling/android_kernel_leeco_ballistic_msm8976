1df0070ce2c9ec8ae219a3c3e86bb1d6b5cf18b3
Peter Hurley <peter@hurleysoftware.com>
2013-05-17 12:49:48 -0400

    tty: Remove TTY_HW_COOK_IN/OUT
    No in-tree tty driver supports cooked mode in hardware; remove.
    
    Signed-off-by: Peter Hurley <peter@hurleysoftware.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/tty.h
+++ include/linux/tty.h
@@ -307,8 +307,6 @@ struct tty_file_private {
 #define TTY_PUSH 		6	/* n_tty private */
 #define TTY_CLOSING 		7	/* ->close() in progress */
 #define TTY_LDISC_OPEN	 	11	/* Line discipline is open */
-#define TTY_HW_COOK_OUT 	14	/* Hardware can do output cooking */
-#define TTY_HW_COOK_IN 		15	/* Hardware can do input cooking */
 #define TTY_PTY_LOCK 		16	/* pty private */
 #define TTY_NO_WRITE_SPLIT 	17	/* Preserve write boundaries to driver */
 #define TTY_HUPPED 		18	/* Post driver->hangup() */
