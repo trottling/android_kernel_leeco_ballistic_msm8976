0a9f7657ed77b1fe12d4e3e43d839432677653dd
Tushar Behera <tushar.behera@linaro.org>
2013-06-17 14:46:13 +0530

    PCI: mvebu: Convert to use devm_ioremap_resource
    Commit 75096579c3ac ("lib: devres: Introduce devm_ioremap_resource()")
    introduced devm_ioremap_resource() and deprecated the use of
    devm_request_and_ioremap().
    
    While at it, modify mvebu_pcie_map_registers() to propagate error code.
    
    Signed-off-by: Tushar Behera <tushar.behera@linaro.org>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Acked-by: Ezequiel Garcia <ezequiel.garcia@free-electrons.com>

--- drivers/pci/host/pci-mvebu.c
+++ drivers/pci/host/pci-mvebu.c
@@ -728,9 +728,9 @@ mvebu_pcie_map_registers(struct platform_device *pdev,
 
 	ret = of_address_to_resource(np, 0, &regs);
 	if (ret)
-		return NULL;
+		return ERR_PTR(ret);
 
-	return devm_request_and_ioremap(&pdev->dev, &regs);
+	return devm_ioremap_resource(&pdev->dev, &regs);
 }
 
 #define DT_FLAGS_TO_TYPE(flags)       (((flags) >> 24) & 0x03)
@@ -874,9 +874,10 @@ static int __init mvebu_pcie_probe(struct platform_device *pdev)
 		}
 
 		port->base = mvebu_pcie_map_registers(pdev, child, port);
-		if (!port->base) {
+		if (IS_ERR(port->base)) {
 			dev_err(&pdev->dev, "PCIe%d.%d: cannot map registers\n",
 				port->port, port->lane);
+			port->base = NULL;
 			continue;
 		}
 
