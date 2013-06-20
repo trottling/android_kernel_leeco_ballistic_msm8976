60648ae572748154657f67771a914a46a001192e
Gavin Shan <shangw@linux.vnet.ibm.com>
2013-06-20 13:20:54 +0800

    powerpc/eeh: Make eeh_pe_get() public
    While processing EEH event interrupt from P7IOC, we need function
    to retrieve the PE according to the indicated EEH device. The patch
    makes function eeh_pe_get() public so that other source files can call
    it for that purpose. Also, the patch fixes referring to wrong BDF
    (Bus/Device/Function) address while searching PE in function
    __eeh_pe_get().
    
    Signed-off-by: Gavin Shan <shangw@linux.vnet.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/eeh.h
+++ arch/powerpc/include/asm/eeh.h
@@ -197,6 +197,7 @@ static inline void eeh_serialize_unlock(unsigned long flags)
 typedef void *(*eeh_traverse_func)(void *data, void *flag);
 int eeh_phb_pe_create(struct pci_controller *phb);
 struct eeh_pe *eeh_phb_pe_get(struct pci_controller *phb);
+struct eeh_pe *eeh_pe_get(struct eeh_dev *edev);
 int eeh_add_to_parent_pe(struct eeh_dev *edev);
 int eeh_rmv_from_parent_pe(struct eeh_dev *edev, int purge_pe);
 void *eeh_pe_dev_traverse(struct eeh_pe *root,
