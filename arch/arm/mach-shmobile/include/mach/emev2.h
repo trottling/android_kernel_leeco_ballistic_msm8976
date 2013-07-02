4affcbacff08e8fb9e14240df6d654b9ff83183b
Magnus Damm <damm@opensource.se>
2013-07-02 18:28:30 +0900

    ARM: shmobile: Disconnect EMEV2 SMP code from clocks
    Update the EMEV2 SMP code to access the SMU directly
    instead of relying on help from the legacy clock code.
    
    This change moves us one step closer to common clocks.
    
    Signed-off-by: Magnus Damm <damm@opensource.se>
    Acked-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- arch/arm/mach-shmobile/include/mach/emev2.h
+++ arch/arm/mach-shmobile/include/mach/emev2.h
@@ -6,7 +6,6 @@ extern void emev2_init_irq(void);
 extern void emev2_add_early_devices(void);
 extern void emev2_add_standard_devices(void);
 extern void emev2_clock_init(void);
-extern void emev2_set_boot_vector(unsigned long value);
 
 #define EMEV2_GPIO_BASE 200
 #define EMEV2_GPIO_IRQ(n) (EMEV2_GPIO_BASE + (n))
