b5f382bd1d56c68602e312af1dc65b3c25a27852
Kevin Hao <haokexin@gmail.com>
2013-07-14 17:02:05 +0800

    powerpc: Make flush_fp_to_thread() nop when CONFIG_PPC_FPU is disabled
    In the current kernel, the function flush_fp_to_thread() is not
    dependent on CONFIG_PPC_FPU. So most invocations of this function
    is not wrapped by CONFIG_PPC_FPU. Even through we don't really
    save the FPRs to the thread struct if CONFIG_PPC_FPU is not enabled,
    but there does have some runtime overhead such as the check for
    tsk->thread.regs and preempt disable and enable. It really make
    no sense to do that. So make it a nop when CONFIG_PPC_FPU is
    disabled. Also remove the wrapped #ifdef CONFIG_PPC_FPU
    when invoking this function.
    
    Signed-off-by: Kevin Hao <haokexin@gmail.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/powerpc/include/asm/switch_to.h
+++ arch/powerpc/include/asm/switch_to.h
@@ -29,7 +29,6 @@ extern void giveup_fpu(struct task_struct *);
 extern void load_up_fpu(void);
 extern void disable_kernel_fp(void);
 extern void enable_kernel_fp(void);
-extern void flush_fp_to_thread(struct task_struct *);
 extern void enable_kernel_altivec(void);
 extern void load_up_altivec(struct task_struct *);
 extern int emulate_altivec(struct pt_regs *);
@@ -45,6 +44,12 @@ extern void discard_lazy_cpu_state(void);
 static inline void discard_lazy_cpu_state(void)
 {
 }
+#endif
+
+#ifdef CONFIG_PPC_FPU
+extern void flush_fp_to_thread(struct task_struct *);
+#else
+static inline void flush_fp_to_thread(struct task_struct *t) { }
 #endif
 
 #ifdef CONFIG_ALTIVEC
