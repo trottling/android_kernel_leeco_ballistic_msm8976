842b9f14acda0f996133fbcb655cc01b92ccdabf
Steffen Klassert <steffen.klassert@secunet.com>
2013-08-28 08:04:14 +0200

    net: Remove FLOWI_FLAG_CAN_SLEEP
    FLOWI_FLAG_CAN_SLEEP was used to notify xfrm about the posibility
    to sleep until the needed states are resolved. This code is gone,
    so FLOWI_FLAG_CAN_SLEEP is not needed anymore.
    
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Change-Id: If44849ba4073cf159cd89907e80606bf531a745d

--- net/ipv6/af_inet6.c
+++ net/ipv6/af_inet6.c
@@ -706,7 +706,7 @@ int inet6_sk_rebuild_header(struct sock *sk)
 					 &final);
 		rcu_read_unlock();
 
-		dst = ip6_dst_lookup_flow(sk, &fl6, final_p, false);
+		dst = ip6_dst_lookup_flow(sk, &fl6, final_p);
 		if (IS_ERR(dst)) {
 			sk->sk_route_caps = 0;
 			sk->sk_err_soft = -PTR_ERR(dst);
