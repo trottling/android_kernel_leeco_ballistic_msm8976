b480c433218ee082f6b38c1a8e40236dc60b95ab
Peter Zijlstra <peterz@infradead.org>
2013-10-03 16:02:23 +0200

    perf: Fix perf_pmu_migrate_context
    While auditing the list_entry usage due to a trinity bug I found that
    perf_pmu_migrate_context violates the rules for
    perf_event::event_entry.
    
    The problem is that perf_event::event_entry is a RCU list element, and
    hence we must wait for a full RCU grace period before re-using the
    element after deletion.
    
    Therefore the usage in perf_pmu_migrate_context() which re-uses the
    entry immediately is broken. For now introduce another list_head into
    perf_event for this specific usage.
    
    This doesn't actually fix the trinity report because that never goes
    through this code.
    
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/n/tip-mkj72lxagw1z8fvjm648iznw@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Change-Id: I411876504fc5e94aa4db75ded94215f9a7a1cd88

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -7523,13 +7523,13 @@ void perf_pmu_migrate_context(struct pmu *pmu, int src_cpu, int dst_cpu)
 				 event_entry) {
 		perf_remove_from_context(event, false);
 		put_ctx(src_ctx);
-		list_add(&event->event_entry, &events);
+		list_add(&event->migrate_entry, &events);
 	}
 
 	synchronize_rcu();
 
-	list_for_each_entry_safe(event, tmp, &events, event_entry) {
-		list_del(&event->event_entry);
+	list_for_each_entry_safe(event, tmp, &events, migrate_entry) {
+		list_del(&event->migrate_entry);
 		if (event->state >= PERF_EVENT_STATE_OFF)
 			event->state = PERF_EVENT_STATE_INACTIVE;
 		perf_install_in_context(dst_ctx, event, dst_cpu);
