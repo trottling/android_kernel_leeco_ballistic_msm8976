e49a58ba545366c7db0b2e5451dd7fbb49232be1
Matthias Brugger <matthias.bgg@gmail.com>
2013-12-05 10:59:57 +0100

    cpufreq_ at32ap-cpufreq.c: Fix section mismatch
    The function at32_cpufreq_driver_init was marked as __init but will be
    called from inside the cpufreq framework. This lead to the following a
    section mismatch during compilation:
    
    WARNING: drivers/built-in.o(.data+0x2448): Section mismatch in reference
    from the variable at32_driver to the function
    .init.text:at32_cpufreq_driver_init()
    The variable at32_driver references
    the function __init at32_cpufreq_driver_init()
    If the reference is valid then annotate the
    variable with __init* or __refdata (see linux/init.h) or name the
    variable:
    *_template, *_timer, *_sht, *_ops, *_probe, *_probe_one, *_console
    
    Signed-off-by: Matthias Brugger <matthias.bgg@gmail.com>

--- drivers/cpufreq/at32ap-cpufreq.c
+++ drivers/cpufreq/at32ap-cpufreq.c
@@ -75,7 +75,7 @@ static int at32_set_target(struct cpufreq_policy *policy,
 	return 0;
 }
 
-static int __init at32_cpufreq_driver_init(struct cpufreq_policy *policy)
+static int at32_cpufreq_driver_init(struct cpufreq_policy *policy)
 {
 	unsigned int frequency, rate, min_freq;
 	int retval, steps, i;
