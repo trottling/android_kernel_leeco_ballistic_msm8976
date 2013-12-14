8f6f1abd97941da73b97828919c85a4cc916b7a2
Hannes Frederic Sowa <hannes@stressinduktion.org>
2013-12-14 05:13:38 +0100

    inet: make no_pmtu_disc per namespace and kill ipv4_config
    The other field in ipv4_config, log_martians, was converted to a
    per-interface setting, so we can just remove the whole structure.
    
    Signed-off-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Change-Id: I71f9bf351521c682c44f9a6b64db7cf6e2d9efc4

--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -70,6 +70,7 @@ struct netns_ipv4 {
 	struct local_ports sysctl_local_ports;
 
 	int sysctl_tcp_ecn;
+	int sysctl_ip_no_pmtu_disc;
 
 	int sysctl_fwmark_reflect;
 	int sysctl_tcp_fwmark_accept;
--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -307,13 +307,6 @@ static struct ctl_table ipv4_table[] = {
 		.extra2		= &ip_ttl_max,
 	},
 	{
-		.procname	= "ip_no_pmtu_disc",
-		.data		= &ipv4_config.no_pmtu_disc,
-		.maxlen		= sizeof(int),
-		.mode		= 0644,
-		.proc_handler	= proc_dointvec
-	},
-	{
 		.procname	= "ip_nonlocal_bind",
 		.data		= &sysctl_ip_nonlocal_bind,
 		.maxlen		= sizeof(int),
@@ -916,6 +909,13 @@ static struct ctl_table ipv4_net_table[] = {
 		.data		= &init_net.ipv4.sysctl_local_ports.range,
 		.mode		= 0644,
 		.proc_handler	= ipv4_local_port_range,
+	},
+	{
+		.procname	= "ip_no_pmtu_disc",
+		.data		= &init_net.ipv4.sysctl_ip_no_pmtu_disc,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec
 	},
 	{
 		.procname	= "fwmark_reflect",
