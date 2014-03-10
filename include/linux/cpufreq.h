4162047f3347e16c4584d54d01ab3da8e55d51cc
Viresh Kumar <viresh.kumar@linaro.org>
2014-03-10 14:53:34 +0530

    cpufreq: Remove cpufreq_generic_exit()
    cpufreq_generic_exit() is empty now and can be deleted.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Change-Id: I1cbcdf890eba1ef51808f22d9d48f0443dad6121

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -572,10 +572,6 @@ unsigned int cpufreq_generic_get(unsigned int cpu);
 int cpufreq_generic_init(struct cpufreq_policy *policy,
 		struct cpufreq_frequency_table *table,
 		unsigned int transition_latency);
-static inline int cpufreq_generic_exit(struct cpufreq_policy *policy)
-{
-	return 0;
-}
 
 /*********************************************************************
  *                         CPUFREQ STATS                             *
