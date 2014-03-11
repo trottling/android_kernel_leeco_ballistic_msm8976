f2bc8223f6d2e10d7aebe491032d1bab3d662f87
Li RongQing <roy.qing.li@gmail.com>
2014-03-11 10:40:08 +0800

    ipv6: ip6_forward: perform skb->pkt_type check at the beginning
    Packets which have L2 address different from ours should be
    already filtered before entering into ip6_forward().
    
    Perform that check at the beginning to avoid processing such packets.
    
    Signed-off-by: Li RongQing <roy.qing.li@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -351,6 +351,9 @@ int ip6_forward(struct sk_buff *skb)
 	if (net->ipv6.devconf_all->forwarding == 0)
 		goto error;
 
+	if (skb->pkt_type != PACKET_HOST)
+		goto drop;
+
 	if (skb_warn_if_lro(skb))
 		goto drop;
 
@@ -359,9 +362,6 @@ int ip6_forward(struct sk_buff *skb)
 				 IPSTATS_MIB_INDISCARDS);
 		goto drop;
 	}
-
-	if (skb->pkt_type != PACKET_HOST)
-		goto drop;
 
 	skb_forward_csum(skb);
 
