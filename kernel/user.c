571e0fbd6e1c4a83e7619ec4575966506a7573cc
Al Viro <viro@zeniv.linux.org.uk>
2014-10-31 22:56:04 -0400

    common object embedded into various struct ....ns
    for now - just move corresponding ->proc_inum instances over there
    
    Acked-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Change-Id: I03c7b77f9b07838c5fb5bcadde61280b8b2f049d

--- kernel/user.c
+++ kernel/user.c
@@ -50,7 +50,7 @@ struct user_namespace init_user_ns = {
 	.count = ATOMIC_INIT(3),
 	.owner = GLOBAL_ROOT_UID,
 	.group = GLOBAL_ROOT_GID,
-	.proc_inum = PROC_USER_INIT_INO,
+	.ns.inum = PROC_USER_INIT_INO,
 	.flags = USERNS_INIT_FLAGS,
 #ifdef CONFIG_KEYS_KERBEROS_CACHE
 	.krb_cache_register_sem =
