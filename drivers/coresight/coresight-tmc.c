53ab9c1f18ece734b053ac4d2672b63f7f4091b3
tImIbreakdown <timifix@gmail.com>
2021-07-30 16:08:42 +0300

    drivers: coresight: update from msm-3.18
    * based on https://github.com/LineageOS/android_kernel_leeco_msm8996/commit/4ab64308d5dd0baffa0e5ab151485d4770f6a64d
    * at the time of commit https://github.com/LineageOS/android_kernel_leeco_msm8996/commit/08de8022e3b2be9bcaaa68a695e17a6b2f17f9e8
    
    Change-Id: I72d72152ae340b6bf9d57d51937f3be607274013

--- drivers/coresight/coresight-tmc.c
+++ drivers/coresight/coresight-tmc.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -207,7 +207,6 @@ struct tmc_drvdata {
 	uint32_t		mem_size;
 	bool			sticky_enable;
 	bool			force_reg_dump;
-	bool			force_alloc_from_dma_region;
 	bool			dump_reg;
 	bool			sg_enable;
 	enum tmc_etr_mem_type	mem_type;
@@ -437,12 +436,7 @@ static int tmc_etr_sg_tbl_alloc(struct tmc_drvdata *drvdata)
 	int total_ents = DIV_ROUND_UP(drvdata->size, PAGE_SIZE);
 	int ents_per_blk = PAGE_SIZE/sizeof(uint32_t);
 
-	if (drvdata->force_alloc_from_dma_region)
-		virt_pgdir = (uint32_t *)
-			      get_zeroed_page(GFP_KERNEL | GFP_DMA);
-	else
-		virt_pgdir = (uint32_t *)get_zeroed_page(GFP_KERNEL);
-
+	virt_pgdir = (uint32_t *)get_zeroed_page(GFP_KERNEL);
 	if (!virt_pgdir)
 		return -ENOMEM;
 
@@ -452,12 +446,7 @@ static int tmc_etr_sg_tbl_alloc(struct tmc_drvdata *drvdata)
 		last_pte = ((i + ents_per_blk) > total_ents) ?
 			   total_ents : (i + ents_per_blk);
 		while (i < last_pte) {
-			if (drvdata->force_alloc_from_dma_region)
-				virt_pte = (uint32_t *)
-					get_zeroed_page(GFP_KERNEL | GFP_DMA);
-			else
-				virt_pte = (uint32_t *)
-						get_zeroed_page(GFP_KERNEL);
+			virt_pte = (void *)get_zeroed_page(GFP_KERNEL);
 			if (!virt_pte) {
 				ret = -ENOMEM;
 				goto err;
@@ -861,14 +850,6 @@ static int tmc_enable(struct tmc_drvdata *drvdata, enum tmc_mode mode)
 		return ret;
 
 	mutex_lock(&drvdata->usb_lock);
-	spin_lock_irqsave(&drvdata->spinlock, flags);
-	if (drvdata->reading) {
-		ret = -EBUSY;
-		spin_unlock_irqrestore(&drvdata->spinlock, flags);
-		goto err0;
-	}
-	spin_unlock_irqrestore(&drvdata->spinlock, flags);
-
 	if (drvdata->config_type == TMC_CONFIG_TYPE_ETB) {
 		coresight_cti_map_trigout(drvdata->cti_flush, 1, 0);
 		coresight_cti_map_trigin(drvdata->cti_reset, 2, 0);
@@ -915,6 +896,10 @@ static int tmc_enable(struct tmc_drvdata *drvdata, enum tmc_mode mode)
 	}
 
 	spin_lock_irqsave(&drvdata->spinlock, flags);
+	if (drvdata->reading) {
+		ret = -EBUSY;
+		goto err1;
+	}
 
 	if (drvdata->config_type == TMC_CONFIG_TYPE_ETB) {
 		__tmc_etb_enable(drvdata);
@@ -944,6 +929,11 @@ static int tmc_enable(struct tmc_drvdata *drvdata, enum tmc_mode mode)
 
 	dev_info(drvdata->dev, "TMC enabled\n");
 	return 0;
+err1:
+	spin_unlock_irqrestore(&drvdata->spinlock, flags);
+	if (drvdata->config_type == TMC_CONFIG_TYPE_ETR)
+		if (drvdata->out_mode == TMC_ETR_OUT_MODE_USB)
+			usb_qdss_close(drvdata->usbch);
 err0:
 	mutex_unlock(&drvdata->usb_lock);
 	clk_disable_unprepare(drvdata->clk);
@@ -1338,7 +1328,6 @@ static int tmc_read_prepare(struct tmc_drvdata *drvdata)
 	unsigned long flags;
 	enum tmc_mode mode;
 
-	mutex_lock(&drvdata->usb_lock);
 	spin_lock_irqsave(&drvdata->spinlock, flags);
 	if (!drvdata->sticky_enable) {
 		dev_err(drvdata->dev, "enable tmc once before reading\n");
@@ -1369,13 +1358,11 @@ static int tmc_read_prepare(struct tmc_drvdata *drvdata)
 out:
 	drvdata->reading = true;
 	spin_unlock_irqrestore(&drvdata->spinlock, flags);
-	mutex_unlock(&drvdata->usb_lock);
 
 	dev_info(drvdata->dev, "TMC read start\n");
 	return 0;
 err:
 	spin_unlock_irqrestore(&drvdata->spinlock, flags);
-	mutex_unlock(&drvdata->usb_lock);
 	return ret;
 }
 
@@ -1557,12 +1544,8 @@ static ssize_t tmc_read(struct file *file, char __user *data, size_t len,
 {
 	struct tmc_drvdata *drvdata = container_of(file->private_data,
 						   struct tmc_drvdata, miscdev);
-	char *bufp, *end;
-
-	mutex_lock(&drvdata->usb_lock);
-
-	bufp  = drvdata->buf + *ppos;
-	end = (char *)(drvdata->vaddr + drvdata->size);
+	char *bufp = drvdata->buf + *ppos;
+	char *end = (char *)(drvdata->vaddr + drvdata->size);
 
 	if (*ppos + len > drvdata->size)
 		len = drvdata->size - *ppos;
@@ -1589,7 +1572,6 @@ static ssize_t tmc_read(struct file *file, char __user *data, size_t len,
 
 	if (copy_to_user(data, bufp, len)) {
 		dev_dbg(drvdata->dev, "%s: copy_to_user failed\n", __func__);
-		mutex_unlock(&drvdata->usb_lock);
 		return -EFAULT;
 	}
 
@@ -1597,8 +1579,6 @@ static ssize_t tmc_read(struct file *file, char __user *data, size_t len,
 out:
 	dev_dbg(drvdata->dev, "%s: %zu bytes copied, %d bytes left\n",
 		__func__, len, (int) (drvdata->size - *ppos));
-
-	mutex_unlock(&drvdata->usb_lock);
 	return len;
 }
 
@@ -2413,10 +2393,6 @@ static int tmc_probe(struct platform_device *pdev)
 	drvdata->force_reg_dump = of_property_read_bool
 				  (pdev->dev.of_node,
 				  "qcom,force-reg-dump");
-
-	drvdata->force_alloc_from_dma_region = of_property_read_bool
-				  (pdev->dev.of_node,
-				  "qcom,force-alloc-from-dma-region");
 
 	devid = tmc_readl(drvdata, CORESIGHT_DEVID);
 	drvdata->config_type = BMVAL(devid, 6, 7);
