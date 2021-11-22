ea59152a9335fb1ef9e910c293a92badfc79214b
tImI Breakdown <timifix@gmail.com>
2021-11-22 15:34:21 +0300

    Revert "tcp: take care of truncations done by sk_filter()"
    This reverts commit 56325d9fb7b138b228577ae99382b70084ab82f3.
    
    Change-Id: I1bf1ff59d5f3e8a41c2fb1558dfe879a873121d0

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1045,7 +1045,6 @@ static inline void tcp_prequeue_init(struct tcp_sock *tp)
 }
 
 bool tcp_prequeue(struct sock *sk, struct sk_buff *skb);
-int tcp_filter(struct sock *sk, struct sk_buff *skb);
 
 #undef STATE_TRACE
 
