84b3f0d648ca06903bc525075d0e1e93fc875f72
tImI Breakdown <timifix@gmail.com>
2021-12-03 11:19:40 +0300

    Revert "BACKPORT: [UPSTREAM] udf: Check component length before reading it"
    This reverts commit f68c8e21ddf09f48fc360085ac971f5101cb208e.

--- fs/udf/symlink.c
+++ fs/udf/symlink.c
@@ -42,17 +42,14 @@ static int udf_pc_to_char(struct super_block *sb, unsigned char *from,
 	tolen--;
 	while (elen < fromlen) {
 		pc = (struct pathComponent *)(from + elen);
-		elen += sizeof(struct pathComponent);
 		switch (pc->componentType) {
 		case 1:
 			/*
 			 * Symlink points to some place which should be agreed
  			 * upon between originator and receiver of the media. Ignore.
 			 */
-			if (pc->lengthComponentIdent > 0) {
-				elen += pc->lengthComponentIdent;
+			if (pc->lengthComponentIdent > 0)
 				break;
-			}
 			/* Fall through */
 		case 2:
 			if (tolen == 0)
@@ -77,9 +74,6 @@ static int udf_pc_to_char(struct super_block *sb, unsigned char *from,
 			/* that would be . - just ignore */
 			break;
 		case 5:
-			elen += pc->lengthComponentIdent;
-			if (elen > fromlen)
-				return -EIO;
 			comp_len = udf_get_filename(sb, pc->componentIdent,
 						    pc->lengthComponentIdent,
 						    p, tolen);
@@ -91,6 +85,7 @@ static int udf_pc_to_char(struct super_block *sb, unsigned char *from,
 			tolen--;
 			break;
 		}
+		elen += sizeof(struct pathComponent) + pc->lengthComponentIdent;
 	}
 	if (p > to + 1)
 		p[-1] = '\0';
