8f3fbfd3a53a646433c3d943bc8b6de811082461
tImI Breakdown <timifix@gmail.com>
2022-01-18 15:36:40 +0300

    Revert "gpio: unregister gpiochip device before removing it"
    This reverts commit 864c329b21a1914105a9e591b49def3f30184b65.

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -1278,8 +1278,6 @@ int gpiochip_remove(struct gpio_chip *chip)
 	int		status = 0;
 	unsigned	id;
 
-	gpiochip_unexport(chip);
-
 	spin_lock_irqsave(&gpio_lock, flags);
 
 	gpiochip_remove_pin_ranges(chip);
@@ -1300,6 +1298,9 @@ int gpiochip_remove(struct gpio_chip *chip)
 	}
 
 	spin_unlock_irqrestore(&gpio_lock, flags);
+
+	if (status == 0)
+		gpiochip_unexport(chip);
 
 	return status;
 }
